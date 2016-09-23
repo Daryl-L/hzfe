f6a2d693415b1e59d1b61062d475854e0374a933
xyxiao <463618622@qq.com>
2016-09-23 09:00:25 +0800

    添加背景渐变css hack

--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -19,6 +19,14 @@ export default {
 <style lang="scss">
   body {
     font-family: 'Open Sans', sans-serif;
+    background: rgb(38,143,208);
+    background: -moz-linear-gradient(left, rgba(38,143,208,0.8) 0%, rgba(95,86,172,0.8) 100%);
+    background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(38,143,208,0.8)), color-stop(100%,rgba(95,86,172,0.8)));
+    background: -webkit-linear-gradient(left, rgba(38,143,208,0.8) 0%,rgba(95,86,172,0.8) 100%);
+    background: -o-linear-gradient(left, rgba(38,143,208,0.8) 0%,rgba(95,86,172,0.8) 100%);
+    background: -ms-linear-gradient(left, rgba(38,143,208,0.8) 0%,rgba(95,86,172,0.8) 100%);
+    background: linear-gradient(to right, rgba(38,143,208,0.8) 0%,rgba(95,86,172,0.8) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#268fd0', endColorstr='#5f56ac',GradientType=1 );
     background: linear-gradient(to right,  rgba(38,143,208,0.8) 0%,rgba(95,86,172,0.8) 100%);
   }
 
